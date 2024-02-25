00be08b57e3d5c9090eadadf1b81a0eb63f57c00
phongnh <phongnh106@gmail.com>
2024-02-25 23:46:11 +0700

    update

--- context-api-demo/src/components/Searchbar/Searchbar.jsx
+++
@@ -1,8 +0,0 @@
-export default function Searchbar() {
-  return (
-    <div className="mb-4">
-      {/* <label htmlFor="search-input" className="form-label">Email address</label> */}
-      <input type="email" className="form-control" id="search-bar" aria-describedby="search-input" placeholder="Search..." />
-    </div>
-  )
-}
